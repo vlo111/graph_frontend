1d4faf5a234f418d5def12273c8cb8fda1ecc4d5
Karen Nikoghosyan <nikoghosyankaren@gmail.com>
2020-06-22 20:58:49 +0000

    Update .deploy-staging.sh

---
+++ .deploy-staging.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get Servers list
+set -f
+string=$STAGE_DEPLOY_SERVER
+array=(${string//,/ })
+
+# Iterate servers for deploy and pull last commit
+for i in "${!array[@]}"; do
+    echo "Deploy project on server ${array[i]}"
+    ssh root@${array[i]} "cd /var/www/graphs-project && git pull && npm install && pm2 restart Frontend"
+done
+
