cbdbbf161b54cfa338ecc65f9a1fb72367bfe1ee
Tigran Muradyan <tigran.m@menumend.com>
2020-08-12 19:39:27 +0400

    git reset

--- .deploy-production.sh
+++ .deploy-production.sh
@@ -8,6 +8,6 @@ array=(${string//,/ })
 # Iterate servers for deploy and pull last commit
 for i in "${!array[@]}"; do
     echo "Deploy project on server ${array[i]}"
-    ssh root@${array[i]} "cd /var/www/prod/graphs-project && git pull && yarn && yarn build"
+    ssh root@${array[i]} "cd /var/www/prod/graphs-project && git reset --hard && git pull && yarn && yarn build"
 done
 
