04479d690659f8c0643204cae330df42d9cac4aa
Tigran Muradyan <tigran.m@menumend.com>
2020-08-14 23:03:35 +0400

    fix fontawesome icons

--- src/components/form/Icon.js
+++ src/components/form/Icon.js
@@ -20,7 +20,8 @@ class Icon extends Component {
     }
     return (
       <span className={`icon ${className}`} {...props}>
-        {_.isString(value) ? (<img src={value} alt="icon" />) : null}
+        {_.isString(value) && value.startsWith('fa') ? (<span className={`icon fa ${value}`} />) : null}
+        {_.isString(value) && !value.startsWith('fa-') ? (<img src={value} alt="icon" />) : null}
         {_.isObjectLike(value) ? value : null}
       </span>
     );
