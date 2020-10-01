8723e41ba2dadcf8e212da955968580024a0f1b2
Tigran Muradyan <tigran.m@menumend.com>
2020-10-01 18:13:44 +0400

    install fix

--- .deploy-staging.sh
+++ .deploy-staging.sh
@@ -8,6 +8,6 @@ array=(${string//,/ })
 # Iterate servers for deploy and pull last commit
 for i in "${!array[@]}"; do
     echo "Deploy project on server ${array[i]}"
-    ssh root@${array[i]} "cd /var/www/graphs-project && git reset --hard && git pull && yarn && postinstall && pm2 restart Frontend-dev"
+    ssh root@${array[i]} "cd /var/www/graphs-project && git reset --hard && git pull && yarn && pm2 restart Frontend-dev"
 done
 
