999dc1628faef2a1eab3ade98cdb4bc3e218c103
Tigran Muradyan <tigran.m@menumend.com>
2020-10-02 17:56:22 +0400

    added link colors and node color

--- src/Chart.js
+++ src/Chart.js
@@ -790,6 +790,7 @@ class Chart {
         keywords: d.keywords || [],
         location: d.location || undefined,
         labels: d.labels,
+        color: d.color || ChartUtils.nodeColor()(d),
       }));
     }
     if (show) {
@@ -825,6 +826,7 @@ class Chart {
           type: pd.type || d.type || '',
           direction: pd.direction || d.direction || '',
           hidden: pd.hidden || d.hidden,
+          color: d.color || ChartUtils.linkColor()(d),
         };
       });
     }
