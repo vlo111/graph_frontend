0a3ce6d06bc07b7d33665fdc2c118e5d35d40d67
davoasrn <davoasrn@gmail.com>
2020-10-05 00:31:56 -0700

    shared users list show fixing

--- src/components/GraphListFooter.js
+++ src/components/GraphListFooter.js
@@ -11,44 +11,56 @@ import { getActionsCountRequest } from '../store/actions/graphs';
 import Button from './form/Button';
 import ShareTooltip from './ShareTooltip';
 
-const TootlipContent = ({graphId}) => <Suspense fallback={<div>Loading...</div>}>
-<ShareTooltip graphId={graphId} />
-</Suspense>;
+const TootlipContent = ({ graphId }) => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <ShareTooltip graphId={graphId} />
+  </Suspense>
+);
 TootlipContent.propTypes = {
-    graphId: PropTypes.number.isRequired,
-}
+  graphId: PropTypes.number.isRequired,
+};
 
-const GraphListFooter = ({graphId}) => {
-    const actionsCountAll = useSelector(getActionsCount);
-    const actionsCount = actionsCountAll[graphId];
-    const dispatch = useDispatch();
+const GraphListFooter = ({ graphId }) => {
+  const actionsCountAll = useSelector(getActionsCount);
+  const actionsCount = actionsCountAll[graphId];
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (graphId) {
-            dispatch(getActionsCountRequest(graphId));
-        }
-    }, [dispatch, graphId]);
+  useEffect(() => {
+    if (graphId) {
+      dispatch(getActionsCountRequest(graphId));
+    }
+  }, [dispatch, graphId]);
 
-    return <div className='graphListFooter'>
-        <Button icon={<HeartSvg style={{ height: 14 }} />} className="transparent footer-icon" >
-            <span className='graphListFooter__count'>{actionsCount?.likes}</span>
-        </Button>
-        <Button icon={<CommentSvg style={{ height: 14, color: 'red' }} />} className="transparent footer-icon">
-            <span className='graphListFooter__count'>{actionsCount?.comments}</span>
-        </Button>
-        <Button icon={<ViewPassSvg style={{ height: 14 }} />} className="transparent footer-icon">
-            <span className='graphListFooter__count'>{actionsCount?.views}</span>
-        </Button>
-        <Tooltip overlay={<TootlipContent graphId={graphId} />} trigger={['click']}>
+  return (
+    <div className="graphListFooter">
+      <Button icon={<HeartSvg style={{ height: 14 }} />} className="transparent footer-icon">
+        <span className="graphListFooter__count">{actionsCount?.likes}</span>
+      </Button>
+      <Button icon={<CommentSvg style={{ height: 14, color: 'red' }} />} className="transparent footer-icon">
+        <span className="graphListFooter__count">{actionsCount?.comments}</span>
+      </Button>
+      <Button icon={<ViewPassSvg style={{ height: 14 }} />} className="transparent footer-icon">
+        <span className="graphListFooter__count">{actionsCount?.views}</span>
+      </Button>
+      {actionsCount?.shares
+        ? (
+          <Tooltip overlay={<TootlipContent graphId={graphId} />} trigger={['hover']}>
             <Button icon={<ShareSvg style={{ height: 14 }} />} className="transparent footer-icon">
-                <span className='graphListFooter__count'>{actionsCount?.shares}</span>
+              <span className="graphListFooter__count">{actionsCount?.shares}</span>
             </Button>
-        </Tooltip>
+          </Tooltip>
+        )
+        : (
+          <Button icon={<ShareSvg style={{ height: 14 }} />} className="transparent footer-icon">
+            <span className="graphListFooter__count">{actionsCount?.shares}</span>
+          </Button>
+)}
     </div>
+  );
 };
 
 GraphListFooter.propTypes = {
-    graphId: PropTypes.number.isRequired,
-}
+  graphId: PropTypes.number.isRequired,
+};
 
 export default React.memo(GraphListFooter);
