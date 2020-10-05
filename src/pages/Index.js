be8469f61ada8ac6ab4572991c49532c9e17522b
Tigran Muradyan <tigran.m@menumend.com>
2020-10-05 18:42:56 +0400

    fixes

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,61 +5,61 @@ import queryString from 'query-string';
 import memoizeOne from 'memoize-one';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import {
+  Tabs, Tab,
+  Col, Row, Nav, NavItem, Container as Containers,
+} from 'react-bootstrap';
 import { getGraphsListRequest } from '../store/actions/graphs';
 import Wrapper from '../components/Wrapper';
 import Button from '../components/form/Button';
 import Utils from '../helpers/Utils';
 import Pagination from '../components/Pagination';
 import Header from '../components/Header';
-import { Tabs, Tab } from 'react-bootstrap';
-import {Col, Row, Nav, NavItem, Container as Containers } from 'react-bootstrap' 
+
 import GraphTemplates from './profile/GraphTemplates';
-import Shared from '../pages/Shared';
-import Home from './Home'; 
+import Shared from './Shared';
+import Home from './Home';
 
-class Index extends Component { 
-
+class Index extends Component {
   render() {
-   
     return (
-        <div className="container">
-            <Header />
-                    
-             <div className="indexTabs">
-             <Containers style={{ width: "auto" }}>
-                    <Tab.Container id="left-tabs-example" defaultActiveKey="first" role="tabpanel">
-                    
-                    <Row  style={{ padding: "1em 1em", background: "#EEEEEE" }}>
-                        <Col sm={2}>
-                        <Nav variant="pills" className="flex-column">
-                            <Nav.Item>
-                            <Nav.Link eventKey="first">Home</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                            <Nav.Link eventKey="second">Templates</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                            <Nav.Link eventKey="third" icon="fa-pencil">Shared Graphs</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                        </Col>
-                        <Col sm={10}>
-                        <Tab.Content className="tab-content">
-                            <Tab.Pane eventKey="first" style={{ height: "90vh" }}>
-                            <Home />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="second" style={{ height: "90vh" }}>
-                            <GraphTemplates />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="third">
-                            <Shared />
-                            </Tab.Pane>
-                        </Tab.Content>
-                        </Col>
-                    </Row>
-                    </Tab.Container> 
-                    </Containers>
-             </div> 
+      <div className="container">
+        <Header />
+
+        <div className="indexTabs">
+          <Containers style={{ width: 'auto' }}>
+            <Tab.Container id="left-tabs-example" unmountOnExit defaultActiveKey="first" role="tabpanel">
+              <Row style={{ padding: '1em 1em', background: '#EEEEEE' }}>
+                <Col sm={2}>
+                  <Nav variant="pills" className="flex-column">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Templates</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third" icon="fa-pencil">Shared Graphs</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Col>
+                <Col sm={10}>
+                  <Tab.Content className="tab-content">
+                    <Tab.Pane eventKey="first" style={{ height: '90vh' }}>
+                      <Home />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second" style={{ height: '90vh' }}>
+                      <GraphTemplates />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <Shared />
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
+          </Containers>
+        </div>
       </div>
     );
   }
