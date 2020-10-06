ea14a7cfd811d1d204fd52cadc2d13258242e3ec
Tigran Muradyan <tigran.m@menumend.com>
2020-10-06 18:22:11 +0400

    UI fixes

--- src/components/VerticalTabs.js
+++ src/components/VerticalTabs.js
@@ -18,6 +18,9 @@ class VerticalTabs extends Component {
 
   setActiveTab = (index) => {
     this.setState({ index });
+    if (this.props.onChange) {
+      this.props.onChange(index);
+    }
   }
 
   render() {
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
 import Wrapper from '../components/Wrapper';
 import Header from '../components/Header';
 
@@ -8,11 +9,22 @@ import Home from './Home';
 import VerticalTabs from '../components/VerticalTabs';
 
 class Index extends Component {
+  handleChange = () => {
+    const queryObj = queryString.parse(window.location.search);
+    delete queryObj.page;
+    const query = queryString.stringify(queryObj);
+    this.props.history.replace(`?${query}`);
+  }
+
   render() {
     return (
       <Wrapper className="homePage">
         <Header />
-        <VerticalTabs className="homePageTabs" tabs={['Home', 'Templates', 'Shared Graphs']}>
+        <VerticalTabs
+          className="homePageTabs"
+          tabs={['Home', 'Templates', 'Shared Graphs']}
+          onChange={this.handleChange}
+        >
           <Home />
           <GraphTemplates />
           <Shared />
