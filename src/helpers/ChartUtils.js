79b798d9bdb8312e4e60590bcf4691c355b3e7b9
Tigran Muradyan <tigran.m@menumend.com>
2020-10-08 17:05:19 +0400

    color fix

--- src/helpers/ChartUtils.js
+++ src/helpers/ChartUtils.js
@@ -261,9 +261,9 @@ class ChartUtils {
   static nodeColor = (d) => {
     if (!this.nodeColorObj[d.type]) {
       if (d.color) {
-        // this.nodeColorsArr = this.nodeColorsArr.filter((c) => d.color !== c);
-        // this.nodeColorObj[d.type] = d.color;
-        // return d.color;
+        this.nodeColorsArr = this.nodeColorsArr.filter((c) => d.color !== c);
+        this.nodeColorObj[d.type] = d.color;
+        return d.color;
       }
       this.nodeColorObj[d.type] = this.nodeColorsArr.shift() || randomColor();
     }
