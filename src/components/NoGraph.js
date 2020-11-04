50ab7fee345ec563f2da5a4d350b67ab3968d831
David <karapetyandavidgm@gmail.com>
2020-11-04 14:52:04 +0400

    change template style
    change empty data result

--- src/components/NoGraph.js
+++ src/components/NoGraph.js
@@ -1,12 +1,12 @@
 import React from 'react'; 
-import noGraphImg from '../assets/images/no-graph.png'; 
+import noGraphSvg from '../assets/images/no_result.svg'; 
 
 export default function  NoGraph() { 
 
   return (
     <div className="no-graphs">
     <h1 className="title">No Graphs ...</h1>
-    <img src={noGraphImg} className="no-graph-img" alt="No Graphs" />
+    <img src={noGraphSvg} className="no-graph-img" alt="No Graphs" />
   </div>
   );
 };  
--- src/pages/profile/GraphTemplates.js
+++ src/pages/profile/GraphTemplates.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import queryString from 'query-string';
 import memoizeOne from 'memoize-one';
 import moment from 'moment';
+import _ from 'lodash'
 import { Link, withRouter } from 'react-router-dom';
 import { getGraphsListRequest } from '../../store/actions/graphs';
 import Pagination from '../../components/Pagination';
@@ -16,22 +17,31 @@ class Home extends Component {
     getGraphsListRequest: PropTypes.func.isRequired,
     graphsList: PropTypes.array.isRequired,
     graphsListInfo: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
+    graphsListStatus: PropTypes.string.isRequired, 
   }
 
-  getGraphsList = memoizeOne((page) => {
-    this.props.getGraphsListRequest(page, { status: 'template' });
+  getGraphsList = memoizeOne((page, s) => {
+    this.props.getGraphsListRequest(page, { s, status: 'template' });
   })
 
 
   render() {
-    const { graphsList, graphsListInfo: { totalPages } } = this.props;
-    const { page = 1 } = queryString.parse(window.location.search);
-    this.getGraphsList(page);
+    const { graphsList, graphsListStatus, graphsListInfo: { totalPages } } = this.props;
+    const { page = 1, s } = queryString.parse(window.location.search);
+    this.getGraphsList(page, s); 
     return (
       <>
         <div className={`graphsList ${!graphsList.length ? 'empty' : ''}`}>
-          {graphsList.length ? graphsList.map((graph) => (
+        {s ? (
+            <h2 className="searchResult">
+              {'Search Result for: '}
+              <span>{s}</span>
+            </h2>
+          ) : null}
+          {graphsListStatus !== 'request' && _.isEmpty(graphsList) ? (
+            <NoGraph />
+          ) : null}
+          {graphsList.map((graph) => (
             <article key={graph.id} className="graphsItem">
               <GraphListHeader graph={graph} />
               <div className="top">
@@ -61,7 +71,7 @@ class Home extends Component {
               </Link>
               <GraphListFooter graph={graph} />
             </article>
-          )) : <NoGraph />}
+           ))}
         </div>
         <Pagination totalPages={totalPages} />
       </>
@@ -70,8 +80,9 @@ class Home extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  graphsList: state.graphs.graphsList,
-  graphsListInfo: state.graphs.graphsListInfo,
+  graphsListStatus: state.graphs.graphsListStatus,
+  graphsList: state.graphs.graphsList || [],
+  graphsListInfo: state.graphs.graphsListInfo || {},
 });
 
 const mapDispatchToProps = {
