1199ffa3e58e8d6f07fac0bace10c90129743ca9
David <karapetyandavidgm@gmail.com>
2020-12-02 12:00:18 +0400

    change logo project

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ class Header extends Component {
       <header id="header">
         <div className="logo-graphs">
           <Link to="/">
-            <h3> graphs analysed </h3>
+            <h3> Araks analysed </h3>
           </Link>
         </div>
         <SearchGraphs />
