9654884cade6931394551f5b3c1ea0c26fe98205
David <karapetyandavidgm@gmail.com>
2020-12-07 10:16:04 +0400

    user data home page

--- src/components/PageTabs.js
+++ src/components/PageTabs.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Button from './form/Button';
 
 class PageTabs extends Component {
@@ -30,12 +31,37 @@ class PageTabs extends Component {
 
   render() {
     const {
-      children, tabs, location, history, match, className, direction, ...props
+      children, tabs, location, history, match, myAccount, className, direction, ...props
     } = this.props;
+   
     const tab = tabs.find((t) => t.to === location.pathname);
     const list = direction === 'vertical' ? _.reverse([...tabs]) : tabs;
+
+    const checkHomeTabs = !!((
+      tab.name === 'Home'
+        || tab.name === 'Friends'
+        || tab.name === 'Shared Graphs'
+        || tab.name === 'Templates'));
+
     return (
-      <div id="verticalTabs" className={`${direction} ${className}`} {...props}>
+    // <div id="verticalTabs" className={`${direction} ${className}`} {...props}>
+      <div className={checkHomeTabs ? 'homePageTabs' : 'verticalTabs'} {...props}>
+        {checkHomeTabs ? (
+          <div className="userPanel">
+            <img src={myAccount.avatar} alt="" />
+            <h4>
+              {myAccount.firstName}
+              {' '}
+              {myAccount.lastName}
+            </h4>
+            <a href={myAccount.website} target="_blank">{myAccount.website}</a>
+            <p>
+              {myAccount.firstName}
+              {' '}
+              {myAccount.bio}
+            </p>
+          </div>
+        ) : <div />}
         <ul className="tabsList">
           {list.filter((t) => !t.hidden).map((t) => (
             <li key={t.name} className={`item ${t.to === location.pathname ? 'active' : ''}`}>
@@ -53,4 +79,18 @@ class PageTabs extends Component {
   }
 }
 
-export default withRouter(PageTabs);
+// export default withRouter(PageTabs);
+
+const mapStateToProps = (state) => ({
+  myAccount: state.account.myAccount,
+});
+
+const mapDispatchToProps = {
+};
+
+const Container = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PageTabs);
+
+export default withRouter(Container);
