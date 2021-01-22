a0b91ba62c1c63e463018525f945a907f4c9a011
Tigran Muradyan <tigran.m@menumend.com>
2021-01-22 18:17:03 +0400

    fixes

--- src/pages/GraphCompare.js
+++ src/pages/GraphCompare.js
@@ -134,7 +134,7 @@ class GraphCompare extends Component {
         node1 = ChartUtils.merge(node2, node1);
       }
 
-      singleGraph.labels.filter((l) => node1.labels?.includes(l.id)).forEach(labels.add, labels);
+      singleGraph.labels.filter((l) => node1.labels?.includes(l.id) && l.type !== 'folder').forEach(labels.add, labels);
       return node1;
     });
 
