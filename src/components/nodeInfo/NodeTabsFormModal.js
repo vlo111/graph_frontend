c06c6071d5e1548247c3a411bfb16b9bb0f7c12a
analysed soft <analysedsoft@gmail.com>
2021-02-02 09:53:47 +0400

    bug fix , remove iframe in tabs

--- src/components/nodeInfo/NodeTabsFormModal.js
+++ src/components/nodeInfo/NodeTabsFormModal.js
@@ -69,26 +69,12 @@ class NodeTabsFormModal extends Component {
     this.setState({ tabData });
   }
 
-  controlIFrame = (html) => {
-    const path = '^<\\s*p[^>]*><\\s*a[^>]*>(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)<\\s*/\\s*a><\\s*/\\s*p>$';
-
-    if (html.match(path)) {
-      const link = html.substring(html.indexOf('>http') + 1, html.length - 8);
-
-      return `<iframe width="100%" height="100%" src="${link}" />`;
-    }
-
-    return html;
-  }
-
   save = async () => {
     const {
       node, customField, customFields, fieldName, currentUserId,
     } = this.props;
     const isUpdate = !!fieldName;
     const { tabData, errors } = this.state;
-
-    tabData.content = this.controlIFrame(tabData.content);
 
     tabData.documents = tabData.documents?.sort((x, y) => {
       const first = x.file.type.includes('image');
