73013466eae83ab6a1158d1c01eb3843f3b92d48
analysed soft <analysedsoft@gmail.com>
2021-02-03 16:18:47 +0400

    bug fix

--- src/components/GraphUsersInfo.js
+++ src/components/GraphUsersInfo.js
@@ -25,8 +25,7 @@ class GraphUsersInfo extends Component {
       firstName: '',
       lastName: '',
     };
-    const updatedUser = singleGraph.users.find((u) => +u.id === +(node.updatedUser || singleGraph.userId));
-    console.log(node, 'node', createdUser, 'createdUser', updatedUser, 'updatedUser');
+    const updatedUser = singleGraph.users.find((u) => +u.id === +(node.updatedUser || singleGraph.userId)); 
     return (
       <Modal
         isOpen
