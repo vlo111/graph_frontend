2ded510714dfd053c04b99029349df4d0e222894
analysed soft <analysedsoft@gmail.com>
2021-03-01 10:38:26 +0400

    add map style file

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ class Header extends Component {
         <div className="start-graphs">
           <div className="buttonsWrapper">
             <Button color="accent" onClick={this.startGraph}>
-              Start a graph aaaaaaa
+              Start a graph
             </Button>
           
               <Button color="accent" onClick={this.compareGraph}>
