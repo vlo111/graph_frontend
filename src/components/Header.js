a6e721400956c8147939148cf4371944a2d2a660
analysed soft <analysedsoft@gmail.com>
2021-03-03 14:16:02 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ class Header extends Component {
         <div className="start-graphs">
           <div className="buttonsWrapper">
             <Button color="accent" onClick={this.startGraph}>
-              Start a graph
+              Start a graph  eeee
             </Button>
           
               <Button color="accent" onClick={this.compareGraph}>
