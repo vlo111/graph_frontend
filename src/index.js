fbba335b1428f1a21cb3df830ba976655145dac5
Tigran Muradyan <tigran.m@menumend.com>
2021-04-06 17:27:18 +0400

    ie9 support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'react-app-polyfill/jsdom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
