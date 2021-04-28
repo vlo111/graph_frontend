8f359a075cc9005073864a47b415c15036752fdf
analysed soft <analysedsoft@gmail.com>
2021-04-28 17:41:06 +0400

    change style home page change open tools graphs in home page

--- src/components/GraphListHeader.js
+++ src/components/GraphListHeader.js
@@ -5,22 +5,19 @@ import { useDispatch } from 'react-redux';
 import { useHistory } from "react-router-dom";
 import PropTypes from 'prop-types';
 import queryString from 'query-string';
-
-import { toast } from 'react-toastify';
+ import Popover from './form/Popover';
 import Button from './form/Button';
-import { updateGraphRequest } from '../store/actions/shareGraphs';
+import { toast } from 'react-toastify'; 
 import UpdateGraphModal from './chart/UpdateGraphModal';
 import { deleteGraphRequest } from '../store/actions/graphs';
 import {deleteGraphRequest as DeleteShareGraphRequest} from '../store/actions/shareGraphs';
-
 import { ReactComponent as TrashSvg } from '../assets/images/icons/trash.svg';
-import { ReactComponent as EditSvg } from '../assets/images/icons/edit.svg';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import { ReactComponent as EditSvg } from '../assets/images/icons/edit.svg'; 
 import { getGraphsListRequest } from '../store/actions/graphs';
 
 const GraphListHeader = ({ graph, headerTools }) => {
   const dispatch = useDispatch();
-  const [openEditModal, setOpenEditModal] = useState(false);
+  const [openEditModal, setOpenEditModal] = useState(false); 
   const history = useHistory()
   const { page = 1, s: searchParam } = queryString.parse(window.location.search); 
   const notification = false;
@@ -51,8 +48,7 @@ const GraphListHeader = ({ graph, headerTools }) => {
       history.push("/");
       toast.info('Successfully deleted');
     }
-  }, [dispatch]); 
-  
+  }, [dispatch]);  
   return (
    
     <div className="graphListHeader">
@@ -62,24 +58,24 @@ const GraphListHeader = ({ graph, headerTools }) => {
           onClick={() => handleDeleteShareGraph(graph?.share.id)}
           className="transparent delete" />
       ) : (
-          <DropdownButton
-            iconClass="fa list-ul"
-            alignRight
-            // drop={"left"}
-            title={
-              <span><i className="fa fa-bars fa-ellipsis-v "></i> </span> // fa-fw 
-            }
-            id="dropdown-graphListHeader"
+       <div> 
+          <Popover
+            showArrow
+            triggerNode={<button className="popover-trigger"><span><i className="fa fa-bars fa-ellipsis-v "></i> </span></button>}
+            trigger='click'
           >
-            <Button
-              icon={<EditSvg style={{ height: 30 }} />}
-              className="transparent edit"
-              onClick={() => setOpenEditModal(true)} />
-            <Button
-              icon={<TrashSvg style={{ height: 30 }} />}
-              onClick={deleteGraph}
-              className="transparent delete" />
-          </DropdownButton>
+            <div className="popover-list">
+              <Button
+                  icon={<EditSvg style={{ height: 30 }} />}
+                  className="transparent edit"
+                  onClick={() => setOpenEditModal(true)} /> 
+                <Button
+                  icon={<TrashSvg style={{ height: 30 }} />}
+                  onClick={deleteGraph}
+                  className="transparent delete" />  
+              </div>
+            </Popover>
+         </div>
         )}
       {openEditModal && (
         <UpdateGraphModal
