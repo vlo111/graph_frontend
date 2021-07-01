a963856ebb64267c18ae69269f6069060390223c
Armen Sanoyan <armensanoyan@gmail.com>
2021-07-01 17:27:46 +0400

    :fix grammer mistake for error message

--- src/components/AutoSave.js
+++ src/components/AutoSave.js
@@ -292,11 +292,7 @@ class AutoSave extends Component {
     if (createNodes.length) {
       const { payload: { data = {} } } = await this.props.createNodesRequest(graphId, createNodes);
       if (!_.isEmpty(data.errors)) {
-<<<<<<< HEAD
         toast.error('Something went wrong');
-=======
-        toast.error('Something went vrong');
->>>>>>> 1f930d162c31c1c696e35e410716d44690c4a0d5
       }
     }
     const promise = [];
