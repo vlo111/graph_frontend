846e2a6b44b32c9cb529c5698a5e58ed1641687b
Armen Sanoyan <armensanoyan@gmail.com>
2021-07-02 17:45:09 +0400

    keycode explanations

--- src/data/keyCodes.js
+++ src/data/keyCodes.js
@@ -1,8 +1,11 @@
 export const KEY_CODES = {
-    analytic_code: 65,
-    import_code: 73,
-    data_code: 68,
-    search_code: 70,
-    graphScience_code: 71,
-    media_code: 77,
+    analytic_code: 65, // key code for a
+    import_code: 73, // key code for i
+    data_code: 68, // key code for d
+    search_code: 70, // key code for s
+    graphScience_code: 71, // key code for g
+    media_code: 77, // key code for m
+    label_code: 76, // key code for l
+    label_ellipse_code: 69, // key code for l
+    escape: 27, // key code for l
 }
