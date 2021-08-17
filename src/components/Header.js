a805c02ce07096c948d3390d1a2284e4604e5ab7
analysed soft <analysedsoft@gmail.com>
2021-08-17 11:28:05 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,7 +30,7 @@ class Header extends Component {
             </Button>
           
               <Button color="accent" onClick={this.compareGraph}>
-                Compare graphs rrr
+                Compare graphs
               </Button>
             
           </div>
