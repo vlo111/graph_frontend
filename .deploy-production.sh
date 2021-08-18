4c66c48d7f5b428f62bca60686e752efb09be8f1
analysed soft <analysedsoft@gmail.com>
2021-08-18 14:27:34 +0400

    change repository

--- .deploy-production.sh
+++ .deploy-production.sh
@@ -5,9 +5,10 @@ set -f
 string=$PROD_DEPLOY_SERVER
 array=(${string//,/ })
 
+echo "Deploy start ..."
+
 # Iterate servers for deploy and pull last commit
 for i in "${!array[@]}"; do
     echo "Deploy project on server ${array[i]}"
-    ssh root@${array[i]} "cd /var/www/araks.analysed.ai/graphs-project && git reset --hard origin/graph-save && git checkout graph-save && git pull && yarn && yarn build"
+    ssh root@${array[i]} "cd /var/www/graphs2.analysed.ai/graphs-project && git reset --hard origin/master && git checkout master && git pull && yarn && yarn build"
 done
-
