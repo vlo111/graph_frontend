45891df0ee0cd4121ab8e7bf7bbb022a4a7424ca
analysed soft <analysedsoft@gmail.com>
2021-08-27 17:27:34 +0400

    Add footer page

---
+++ src/components/ToolBarFooter.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+import PropTypes from 'prop-types'; 
+import { getGraphInfoRequest } from '../store/actions/graphs';
+
+
+class ToolBarFooter extends Component {
+  static propTypes = {
+    getGraphInfoRequest: PropTypes.func.isRequired,
+    graphId: PropTypes.string.isRequired,
+    graphInfo: PropTypes.object.isRequired,
+  };   
+
+  render() {
+    const { graphInfo } = this.props;; 
+    return (
+      <>
+        <footer id="graphs-data-info">
+          
+            <div className="nodesMode">
+              <span>
+                {`Nodes (${graphInfo.totalNodes || 0})`}
+              </span>
+            </div>
+            <div className="linksMode">
+              <span>
+                {`Links (${graphInfo.totalLinks || 0})`}
+              </span>
+            </div>
+            <div className="labelsMode">
+              <span>
+                {`Labels (${graphInfo.totalLabels || 0})`}
+              </span>
+            </div> 
+        </footer> 
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  graphInfo: state.graphs.graphInfo, 
+  graphId: state.graphs.singleGraph.id,
+});
+const mapDispatchToProps = {
+  getGraphInfoRequest
+};
+const Container = connect(mapStateToProps, mapDispatchToProps)(ToolBarFooter);
+
+export default withRouter(Container);
