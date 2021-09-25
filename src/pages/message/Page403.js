152c9c142b77eb025be048c9e87bcba56569c9f4
analysed soft <analysedsoft@gmail.com>
2021-09-25 16:45:28 +0400

    #AV20-291
    Graph permission error - After giving view permission user still can edit that graph

---
+++ src/pages/message/Page403.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Page403 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>403</h1>
+      </div>
+    );
+  }
+}
+
+export default Page403;
