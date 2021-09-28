1ea71e4cdfa648ad91dabd07f724f8783299e99b
Hovhannes Davtyan <hovhannes.davtyan@analysed.ai>
2021-09-28 09:43:41 +0300

    error 404 and  share position

--- src/pages/message/Page404.js
+++ src/pages/message/Page404.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-
+import { ReactComponent as LogoSvg } from "../../assets/images/logo.svg";
+import { ReactComponent as NotFound} from "../../assets/images/Found.svg";
+import { ReactComponent as BackSvg } from '../../assets/images/icons/back.svg';
+import Button from "../../components/form/Button";
+import { withRouter, Link } from "react-router-dom";
 class Page404 extends Component {
   render() {
     return (
-      <div>
-        <h1>404</h1>
+
+      <div className="errorPages">
+        <div>
+              <Link to="/" className="logoWrapper permissionLogo">
+                <LogoSvg  />
+                
+              </Link>
+            
+        </div>
+        <div className="permission notFound">
+         <Link to="/" className="backErrorPages">
+              <Button icon={<BackSvg style={{ height: 30 }} />} className="transparent edit"   />
+              Back
+</Link>
+         <NotFound className="notFoundSvg" />
+        </div>
       </div>
     );
   }
