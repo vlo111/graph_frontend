340b812d1af73713e4ffd7f1e23be8307d93e1a2
analysed soft <analysedsoft@gmail.com>
2021-10-21 12:08:51 +0400

    Add reset button in expand forms

--- src/components/graphDashboard/Dashboard.js
+++ src/components/graphDashboard/Dashboard.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import Button from '../form/Button';
 import AddQuery from '../GraphQuery/AddQuery';
 import Queries from '../GraphQuery/Queries';
-
+import { getSingleGraphRequest } from '../../store/actions/graphs';
 import { ReactComponent as SaveSvg } from '../../assets/images/icons/save.svg';
 import { ReactComponent as SettingSvg } from '../../assets/images/icons/setting.svg';
+import { ReactComponent as RefreshSvg } from '../../assets/images/icons/close.svg';
 
 const Dashboard = ({ graph }) => {
   const [showGraphQuery, setshowGraphQuery] = useState(false);
   const [showGraphQuerySetting, setShowGraphQuerySetting] = useState(false);
+  const dispatch = useDispatch();
 
   const toggleGraphQuery = (togle) => {
     setshowGraphQuery(togle);
   };
   const toggleGraphQuerySetting = (togle) => {
     setShowGraphQuerySetting(togle);
+  };
+  const toggleGraphReset = () => {
+    dispatch(getSingleGraphRequest(graph.id));
   };
   return (
     <div className="dashboards">
@@ -30,6 +36,12 @@ const Dashboard = ({ graph }) => {
         onClick={() => toggleGraphQuerySetting(true)}
         title="Save query"
         className="setting_query"
+      />
+      <Button
+        icon={<RefreshSvg style={{ height: 30 }} />}
+        onClick={() => toggleGraphReset()}
+        title="Reset"
+        className="resetAll"
       />
 
       {showGraphQuery ? (
