39685446aef8b790db00a26b49869de7bb6b5e96
vlo111 <vvcsharp@gmail.com>
2021-12-08 05:58:34 -0800

    fix tab iframe and image expand size

--- src/components/tabs/content/Tab.js
+++ src/components/tabs/content/Tab.js
@@ -113,6 +113,7 @@ const Tab = ({
     } else if (!expand) {
       body.className += ' node_expand';
       tabElement.style.width = 'calc(100% - 180px)';
+      getElement('iframe').style.width = '400px !important';
     } else {
       enableEffect();
     }
