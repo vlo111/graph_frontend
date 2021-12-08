60e0f147126471e7271e0acbc89ec7d46e9ab51e
Volodya (*_^) <volodya.vardanyan.93v@gmail.com>
2021-12-09 02:26:00 +0400

    test exam

--- src/pages/sign/UserConfirmation.js
+++ src/pages/sign/UserConfirmation.js
@@ -12,7 +12,7 @@ function UserConfirmation(props) {
 
     if (aipStatus === 'ok') {
       setStatus(status);
-      props.history.replace('/');
+      // props.history.replace('/');
     }
     // else {
     //   props.history.replace('/');
@@ -29,7 +29,7 @@ function UserConfirmation(props) {
 
 UserConfirmation.propTypes = {
   match: PropTypes.any.isRequired,
-  history: PropTypes.string.isRequired,
+  // history: PropTypes.string.isRequired,
 };
 
 export default UserConfirmation;
