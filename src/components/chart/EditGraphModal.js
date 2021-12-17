d3075f8cfc4a6907cb54224fe034b498a6a0c0d0
Analysed Soft <David.Karapetyan1@contracted.pmi.com>
2021-12-17 11:43:57 +0400

    Bug fix in edit graph modal

--- src/components/chart/EditGraphModal.js
+++ src/components/chart/EditGraphModal.js
@@ -50,7 +50,7 @@ const EditGraphModal = ({
     if (!defaultImage && graphId && !publicState) {
       if (!(graph?.nodesPartial?.length > 500)) {
         await updateGraphThumbnailRequest(graphId, svg, 'small');
-        getSingleGraphRequest(graphId);
+        await dispatch(getSingleGraphRequest(graphId));
       }
     }
   }, [graph]);
