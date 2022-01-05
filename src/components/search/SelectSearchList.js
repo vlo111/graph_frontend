07bf1962dc413b36dbde1d81a86abe0f4be4bef0
Analysed Soft <David.Karapetyan1@contracted.pmi.com>
2022-01-05 11:34:25 +0400

    ##AV20-698
    Search optimization

--- src/components/search/SelectSearchList.js
+++ src/components/search/SelectSearchList.js
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '../form/Button';
-
 import { ReactComponent as CloseSvg } from '../../assets/images/icons/close.svg';
 
-const SelectSearchList = ({ search, deleteSelectSearchItem }) => {
-  const [serachData, setSerachData] = useState('');
-  return (
-    <>
-      <div className="select-search-list">
+const SelectSearchList = ({ search, deleteSelectSearchItem }) => (
+  <>
+    <div className="select-search-list">
 
-        <div className="select-search-list__name">
-          {search.search}
-        </div>
-        <div className="select-search-list__title">
-          <Button
-            icon={<CloseSvg style={{ height: 30 }} />}
-            onClick={() => deleteSelectSearchItem()}
-            className="transparent"
-          />
-        </div>
+      <div className="select-search-list__name">
+        {search.search}
       </div>
+      <div className="select-search-list__title">
+        <Button
+          icon={<CloseSvg style={{ height: 30 }} />}
+          onClick={() => deleteSelectSearchItem()}
+          className="transparent"
+        />
+      </div>
+    </div>
 
-    </>
-  );
+  </>
+);
+SelectSearchList.propTypes = {
+  search: PropTypes.array.isRequired,
+  deleteSelectSearchItem: PropTypes.func.isRequired,
 };
+
 export default SelectSearchList;
