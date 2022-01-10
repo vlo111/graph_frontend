e80b556372b2a6e8eaa824d927ff0a7188a914ce
vlo111 <vvcsharp@gmail.com>
2022-01-10 01:24:13 -0800

    update component test

--- src/__test__/component.spec.js
+++ src/__test__/component.spec.js
@@ -5,29 +5,34 @@ import renderWithRedux from './render-redux';
 describe('component', () => {
   let wrapper;
 
-  const form = (id, className) => wrapper.find('#id').find(`.${className}`); // container.querySelector(`form[id="${id}"] > .${className}`);
-
-  const field = (name) => form('createNode', name);
-
-  const expectToBeInputFieldOfTypeText = (formElement, type) => {
-
-    console.log(wrapper.find('#createNode').find('.aaa').type());
-    console.log(wrapper.find('#createNode').find('.aaa').type());
-
-    // expect(formElement).not.toBeNull();
-    // expect(formElement.first()).toEqual('INPUT');
-    // if (type) expect(formElement.type).toEqual(type);
-  };
-
-  const itRendersAsATextBox = (fieldName, type) => {
-    it('renders as a text box', () => {
-      expectToBeInputFieldOfTypeText(field(fieldName), type);
-    });
-  };
-
   beforeEach(() => {
     (wrapper = renderWithRedux(<AddNodeModal />));
   });
 
-  itRendersAsATextBox('aaa');
+  const form = (id, selector) => wrapper.find(`#${id}`).find(`${selector}`); // container.querySelector(`form[id="${id}"] > .${className}`);
+
+  const field = (selector) => form('createNode', selector);
+
+  const expectToBeInputFieldOfTypeText = (formElement, type) => {
+    console.log(formElement.debug());
+    expect(formElement).not.toBeNull();
+    expect(formElement.type()).toEqual('input');
+    if (type) expect(formElement.type).toEqual(type);
+  };
+
+  const itRendersAsATextBox = (selector, type) => {
+    it('renders as a text box', () => {
+      expectToBeInputFieldOfTypeText(field(selector), type);
+    });
+  };
+
+  itRendersAsATextBox('.nodeName > input');
+
+  it('text should be in the modal', () => {
+    const inAdvance = field('.show-more');
+
+    expect(inAdvance.text()).toEqual('Show More');
+
+    inAdvance.simulate('click');
+  });
 });
